ab869b133f177216064ffbee567916b5f1000bb8
jdustindev <jdustindev@gmail.com>
2022-12-22 13:37:56 -0500

    Implement placing X and Y by turn

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -1,21 +1,79 @@
+#BOARD = [
+#    ['_', '|', '_', '|', '_'],
+#    ['_', '|', '_', '|', '_'],
+#    [' ', '|', ' ', '|', ' ']
+#]
+
 BOARD = [
-    ['_', '|', '_', '|', '_'],
-    ['_', '|', '_', '|', '_'],
-    [' ', '|', ' ', '|', ' ']
+    [nil, nil, nil],
+    [nil, nil, nil],
+    [nil, nil, nil],
 ]
 
-player_turn = :x
-winner = nil
+class TicTacToeGame
+    def initialize(board)
+        @board = board.dup
+        @player_turn = 'X'
+    end
 
-def display_board
-    puts BOARD.map {|row| row.join('') }
-end
+    def display_board
+        row1 = [
+            @board[0][0] || '_', '|', @board[0][1] || '_', '|', @board[0][2] || '_'
+        ]
+        row2 = [
+            @board[1][0] || '_', '|', @board[1][1] || '_', '|', @board[1][2] || '_'
+        ]
+        row3 = [
+            @board[2][0] || ' ', '|', @board[2][1] || ' ', '|', @board[2][2] || ' '
+        ]
+        board = [row1, row2, row3]
+        puts board.map {|row| row.join('') }
+    end
 
-until winner
-    system("clear")|| system("cls")
-    display_board
-    input = gets.chomp
-    if  input == 'q'
-        break
+    def parse(input)
+        parsed_input = input.split(',')
+        row = parsed_input[0].to_i - 1
+        column = parsed_input[1].to_i - 1
+        valid = input_valid?(row, column)
+        unless (valid)
+            return "Please input 'row,column' each between 1 and 3 (e.g. '1,1')"
+        end
+        unless space_empty?(row, column)
+            return "Please select a space that is not already taken"
+        end
+        @board[row][column] = @player_turn
+        nil
+    end
+
+    def input_valid?(row, column)
+        row.between?(0,2) && column.between?(0,2)
+    end
+
+    def space_empty?(row, column)
+        @board[row][column] != 'X' && @board[row][column] != 'Y'
+    end
+
+    def play
+        error = nil
+        until @winner
+            system("clear")|| system("cls")
+            puts error
+            display_board
+            error
+            puts "#{@player_turn}'s turn:"
+            input = gets.chomp
+            if  input == 'q'
+                break
+            end
+            error = parse(input)
+            if error
+                puts error
+            else
+                @player_turn == 'X' ? @player_turn = 'Y' : @player_turn = 'X'
+            end
+        end
     end
 end
+
+game = TicTacToeGame.new(BOARD)
+game.play
